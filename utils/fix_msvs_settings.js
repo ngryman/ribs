8a76d83b32fcdb32582fed8e73c00735067d2136
Nicolas Gryman <ngryman@gmail.com>
2013-09-03 19:25:51 -0400

    build: ensure msvrt.dll is used for windows build.

--- utils/fix_msvs_settings.js
+++ utils/fix_msvs_settings.js
@@ -1,6 +1,8 @@
 var fs = require('fs');
 
 var data = fs.readFile('./build/ribs.vcxproj', 'utf8', function(err, data) {
-	var result = data.replace(/<\/Link>/g, '<ImageHasSafeExceptionHandlers>false<\/ImageHasSafeExceptionHandlers><\/Link>');
+	var result = data
+		.replace(/<\/Link>/g, '<ImageHasSafeExceptionHandlers>false<\/ImageHasSafeExceptionHandlers><\/Link>')
+		.replace(/MultiThreaded(?:Debug)?/g, 'MultiThreadedDLL');
 	fs.writeFileSync('./build/ribs.vcxproj', result, 'utf8');
 });
