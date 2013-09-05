11f9fc60405974961e04bd71f46bce3ce53cbb24
Nicolas Gryman <ngryman@gmail.com>
2013-09-05 14:40:59 -0400

    test: refactored resize operation unit tests.

--- test/unit/operations/resize.js
+++ test/unit/operations/resize.js
@@ -10,7 +10,8 @@
  * Module dependencies.
  */
 
-var resize = require('../../../lib/operations/resize');
+var open = require('../../../lib/operations/open'),
+	resize = require('../../../lib/operations/resize');
 
 /**
  * Tests constants.
@@ -20,19 +21,31 @@ var resize = require('../../../lib/operations/resize');
  * Tests helper functions.
  */
 
-var matrixTest = curry(function(expectations, size, done) {
-	async.parallel([
-		resize.bind(null, { width: size }),
-		resize.bind(null, { height: size }),
-		resize.bind(null, { width: size, height: size })
-	], function(err, res) {
-		res.forEach(function(r, i) {
-			r.should.be.instanceof(Image);
-			r.should.have.property('width', expectations[i * 2]);
-			r.should.have.property('height', expectations[(i + 1) * 2]);
+var testResize = curry(function(params, expectedErr, expectedWidth, expectedHeight, done) {
+	open('../fixtures/in-24-a.png', function(err, image) {
+		resize(params, image, function(err, image) {
+			if (expectedErr) {
+				err.should.be.instanceof(Error);
+				err.message.should.equal(expectedErr);
+			}
+			else {
+				should.not.exist(err);
+				image.should.be.instanceof(Image);
+				image.should.have.property('width', expectedWidth);
+				image.should.have.property('height', expectedHeight);
+			}
+			done();
 		});
-		done();
 	});
+});
+
+var matrixTest = curry(function(expect, width, height, done) {
+	var i = 0;
+	async.parallel([
+		testResize({ width: width }, null, expect[i++], expect[i++]),
+		testResize({ height: height }, null, expect[i++], expect[i++]),
+		testResize({ width: width, height: height }, null, expect[i++], expect[i++])
+	], done);
 });
 
 /**
@@ -40,30 +53,28 @@ var matrixTest = curry(function(expectations, size, done) {
  */
 
 describe('resize operation', function() {
-	it('should resize to given width and height', function(done) {
-		ribs.shrink(FILENAME_SRC, WIDTH_2, HEIGHT_2, checkCallbackSize_2(done));
-	});
+	it('should resize to given width and height', testResize({
+		width: 16 / 2,
+		height: 9 / 2 << 0
+	}, null, 16 / 2, 9 / 2 << a));
 
-	it('should resize to given width and height when strings are specified', function(done) {
-		(function() {
-			ribs.shrink(FILENAME_SRC, WIDTH_2.toString(), HEIGHT_2.toString(), checkCallbackSize_2(done));
-		}).should.not.throw();
-	});
+	it('should resize to given width and height when strings are specified', testResize({
+		width: WIDTH_2.toString(),
+		height: HEIGHT_2.toString()
+	}, null, WIDTH_2, HEIGHT_2));
 
-	it('should not upscale when superior sizes are specified', function(done) {
-		async.parallel([
-			asyncCall(FILENAME_SRC, WIDTH * 2, HEIGHT * 2, WIDTH, HEIGHT),
-			asyncCall(FILENAME_SRC, WIDTH * 2, HEIGHT, WIDTH, HEIGHT),
-			asyncCall(FILENAME_SRC, WIDTH, HEIGHT * 2, WIDTH, HEIGHT)
-		], done);
-	});
+	it('should not upscale when superior sizes are specified', matrixTest([
+		80, 0,
+		80, 0,
+		80, 0
+	], WIDTH * 2, HEIGHT * 2));
 
 	it('should keep aspect ratio when sizes compute to a different aspect ratio', function(done) {
 		async.parallel([
-			asyncCall(FILENAME_SRC, WIDTH_2, null, WIDTH_2, HEIGHT_2),
-			asyncCall(FILENAME_SRC, null, HEIGHT_2, WIDTH_2, HEIGHT_2),
-			asyncCall(FILENAME_SRC, WIDTH_2, HEIGHT * 2, WIDTH_2, HEIGHT_2),
-			asyncCall(FILENAME_SRC, WIDTH * 2, HEIGHT_2, WIDTH_2, HEIGHT_2)
+			testResize({ width: WIDTH_2 }, null, WIDTH_2, HEIGHT_2),
+			testResize({ width: HEIGHT_2 }, null, WIDTH_2, HEIGHT_2),
+			testResize({ width: WIDTH_2, height: HEIGHT * 2 }, null, WIDTH_2, HEIGHT_2),
+			testResize({ width: WIDTH * 2, height: HEIGHT_2 }, null, WIDTH_2, HEIGHT_2)
 		], done);
 	});
 
@@ -83,41 +94,41 @@ describe('resize operation', function() {
 		80, 0,
 		80, 0,
 		80, 0
-	], '100-10'));
+	], '100-10', '100-10'));
 
 	it('should resize to the given percentage of source size', matrixTest([
 		80, 0,
 		80, 0,
 		80, 0
-	], 'x50'));
+	], 'x50', 'x50'));
 
 	it('should resize to the given percentage of a constant', matrixTest([
 		50, 28,
 		50, 28,
 		50, 28
-	],'100x50'));
+	], '100x50', '100x50'));
 
 	it('should add a margin to source size given a addition', matrixTest([
 		150, 84,
 		107, 60,
 		107, 60
-	], 'a50'));
+	], 'a50', 'a50'));
 
 	it('should add a margin to a constant given a 2-op addition', matrixTest([
 		107, 60,
 		107, 60,
 		107, 60
-	], '100a50'));
+	], '100a50', '100a50'));
 
 	it('should round down source size to a given multiple', matrixTest([
 		150, 84,
 		107, 60,
 		107, 60
-	], 'r50'));
+	], 'r50', 'r50'));
 
 	it('should round down a constant to a given multiple', matrixTest([
 		107, 60,
 		107, 60,
 		107, 60
-	], '100r50'));
+	], '100r50', '100r50'));
 });
