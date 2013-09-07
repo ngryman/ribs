cc849cca4a94d467288b6ff1da5f035359db6d4c
Nicolas Gryman <ngryman@gmail.com>
2013-09-07 13:21:38 -0400

    fixed msvs fix (weird).

--- utils/fix_msvs_settings.js
+++ utils/fix_msvs_settings.js
@@ -3,6 +3,6 @@ var fs = require('fs');
 var data = fs.readFile('./build/ribs.vcxproj', 'utf8', function(err, data) {
 	var result = data
 		.replace(/<\/Link>/g, '<ImageHasSafeExceptionHandlers>false<\/ImageHasSafeExceptionHandlers><\/Link>')
-		.replace(/MultiThreaded(?:Debug)?/g, 'MultiThreadedDLL');
+		.replace(/MultiThreadedDebug/g, 'MultiThreadedDLL');
 	fs.writeFileSync('./build/ribs.vcxproj', result, 'utf8');
 });
