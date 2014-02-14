af4f82b7d539724e015c043330b59db8a9927888
ngryman <ngryman@gmail.com>
2014-02-13 21:44:51 -0500

    operation class & ported decode operation.

---
+++ src/operation.cc
@@ -0,0 +1,89 @@
+/*!
+ * ribs
+ * Copyright (c) 2013 Nicolas Gryman <ngryman@gmail.com>
+ * LGPL Licensed
+ */
+
+#include "operation.h"
+
+using namespace v8;
+using namespace node;
+using namespace ribs;
+
+namespace ribs {
+
+void ProcessAsync(uv_work_t* req);
+void AfterProcessAsync(uv_work_t* req);
+
+}
+
+NAN_METHOD(Operation::Process) {
+	NanScope();
+
+	NanCallback* callback = NULL;
+
+	// if no callback is specified, return;
+	if (!args[1]->IsFunction()) NanReturnUndefined();
+
+	// if no input is specified we throw a JavaScript exception.
+	if (!CheckInput(args[0])) {
+		return ThrowException(Exception::Error(String::New("invalid input data")));
+	}
+
+	// create callback
+	callback = new NanCallback(args[1].As<Function>());
+
+	// populate baton for further processing
+	Baton* baton     = PreProcess(args);
+	baton->operation = this;
+	baton->callback  = callback;
+	baton->req.data  = baton;
+
+	// here we go!
+	uv_queue_work(uv_default_loop(), &baton->req, ProcessAsync, (uv_after_work_cb)AfterProcessAsync);
+
+	// make the compiler happy ^_^
+	NanReturnUndefined();
+}
+
+void ribs::ProcessAsync(uv_work_t* req) {
+	Baton* baton = static_cast<Baton*>(req->data);
+
+	baton->operation->DoProcess(baton);
+}
+
+void ribs::AfterProcessAsync(uv_work_t* req) {
+	NanScope();
+
+	Baton* baton = static_cast<Baton*>(req->data);
+
+	int argc = 0;
+	Local<Value> argv[2];
+
+	// execute callback with error.
+	// note that we explicitly pass undefined to the 2nd argument.
+	// this is to respect the arity of the function and allow curry for example.
+	if (!baton->error.empty()) {
+		argv[argc++] = Exception::Error(String::New(baton->error.c_str()));
+		argv[argc++] = Local<Value>::New(Undefined());
+	}
+	else {
+		// execute callback with out object
+		argv[argc++] = Local<Value>::New(Null());
+		argv[argc++] = baton->operation->OutputValue(baton);
+	}
+
+	TryCatch tryCatch;
+
+	baton->callback->Call(argc, argv);
+
+	if (tryCatch.HasCaught()) {
+		FatalException(tryCatch);
+	}
+
+	baton->operation->PostProcess(baton);
+
+	delete baton->callback;
+	delete baton->operation;
+	delete baton;
+}
---
+++ src/operation.h
@@ -0,0 +1,33 @@
+/*!
+ * ribs
+ * Copyright (c) 2013 Nicolas Gryman <ngryman@gmail.com>
+ * LGPL Licensed
+ */
+
+#ifndef __RIBS_OPERATION_H__
+#define __RIBS_OPERATION_H__
+
+#include "common.h"
+
+namespace ribs {
+
+class Operation {
+public:
+	NAN_METHOD(Process);
+
+	virtual ~Operation() {}
+
+private:
+	virtual bool CheckInput(v8::Local<v8::Value> in) = 0;
+	virtual Baton* PreProcess(_NAN_METHOD_ARGS) = 0;
+	virtual void DoProcess(Baton* baton) = 0;
+	virtual v8::Local<v8::Object> OutputValue(Baton* baton) = 0;
+	virtual void PostProcess(Baton* baton) = 0;
+
+	friend void ProcessAsync(uv_work_t* req);
+	friend void AfterProcessAsync(uv_work_t* req);
+};
+
+}
+
+#endif
