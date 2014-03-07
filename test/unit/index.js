97006708eff7edc4de9e8fa99aad30cb02cafc6c
ngryman <ngryman@gmail.com>
2014-03-07 17:19:54 -0500

    fix deps bugs.

--- test/unit/index.js
+++ test/unit/index.js
@@ -6,10 +6,11 @@
 
 'use strict';
 
-//require('./pipeline');
+require('./pipeline');
 require('./ribs');
-//require('./utils');
-//require('./operations/open');
-//require('./operations/save');
-//require('./operations/resize');
-//require('./operations/crop');
+require('./utils');
+require('./operations/open');
+require('./operations/save');
+require('./operations/resize');
+require('./operations/crop');
+require('./operations/art');
